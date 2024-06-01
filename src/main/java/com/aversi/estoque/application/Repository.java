982b9deed540b5d71e11622e5db4a045276671e5
aversii <lucasaversi@gmail.com>
2024-06-01 20:15:54 -0300

    feat: Endpoint to getAll produtos are avaiable

--- src/main/java/com/aversi/estoque/application/Repository.java
+++ src/main/java/com/aversi/estoque/application/Repository.java
@@ -1,9 +1,11 @@
 package com.aversi.estoque.application;
 
+import java.util.List;
 
 import com.aversi.estoque.domain.entities.Produto;
 
 public interface Repository {
     Produto cadastrarProduto(Produto produto);
+    List <Produto> listarTodosProdutos ();
     
 }
--- src/main/java/com/aversi/estoque/infra/controller/ProdutoController.java
+++ src/main/java/com/aversi/estoque/infra/controller/ProdutoController.java
@@ -4,25 +4,44 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.aversi.estoque.application.CriarProduto;
+import com.aversi.estoque.application.ListarProduto;
 import com.aversi.estoque.domain.entities.Produto;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/produtos")
 public class ProdutoController {
 
     private CriarProduto criarProduto;
+    private ListarProduto listarProduto;
 
-    public ProdutoController(CriarProduto criarProduto) {
+    public ProdutoController(CriarProduto criarProduto, ListarProduto listarProduto) {
         this.criarProduto = criarProduto;
+        this.listarProduto = listarProduto;
     }
 
-   @PostMapping
-   public ProdutoDto cadastrarProduto(@RequestBody ProdutoDto dto) {
-    Produto salvo = criarProduto.cadastraProduto(new Produto(dto.id(), dto.nome(), dto.quantidade(), dto.unidadeMedida()));
-       return new ProdutoDto(salvo.getId(), salvo.getNome(), salvo.getQuantidade(), salvo.getUnidadeMedida());
-   }
+    @PostMapping
+    public ProdutoDto cadastrarProduto(@RequestBody ProdutoDto dto) {
+        Produto salvo = criarProduto
+                .cadastraProduto(new Produto(dto.id(), dto.nome(), dto.quantidade(), dto.unidadeMedida()));
+        return new ProdutoDto(salvo.getId(), salvo.getNome(), salvo.getQuantidade(), salvo.getUnidadeMedida());
+    }
+
+    @GetMapping
+    public List<ProdutoDto> obterTodosProdutos() {
+        return listarProduto.obterTodosProdutos().stream()
+                .map(produto -> new ProdutoDto(produto.getId(), produto.getNome(), produto.getQuantidade(),
+                        produto.getUnidadeMedida()))
+                .collect(Collectors.toList());
+    }
 
 }
